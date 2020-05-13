e82ea42ca505efa8f89c5822d51303663b12e74f
Joshua Campbell <joshua.campbell@priv.gc.ca>
2020-05-13 13:56:18 -0400

    Added unit test for TagRegistrationPlugin

---
+++ src/Plugins/Components.Support.Tests/Entities/Placeholder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Collections.Generic;
+
+namespace Components.Support.Tests.Entities
+{
+    [EntityLogicalName("placeholder")]
+    internal class Placeholder : Entity
+    {
+        public const string EntityLogicalName = "placeholder";
+
+        [AttributeLogicalName("placeholderid")]
+        public Guid? placeholderid
+        {
+            get { return GetAttributeValue<Guid?>("placeholderid"); }
+            set { base.Id = value.HasValue ? value.Value : Guid.Empty; }
+        }
+
+        [AttributeLogicalName("tags")]
+        public string tags
+        {
+            get { return GetAttributeValue<string>("tags"); }
+            set { SetAttributeValue("tags", value); }
+        }
+
+        [RelationshipSchemaName("placeholder_tags")]
+        public IEnumerable<Tag> placeholder_tags
+        {
+            get { return GetRelatedEntities<Tag>("placeholder_tags", null); }
+            set { SetRelatedEntities("placeholder_tags", null, value); }
+        }
+
+        public Placeholder() :
+                base(EntityLogicalName)
+        {
+        }
+    }
+}
---
+++ src/Plugins/Components.Support.Tests/TagRegistrationPluginTests.cs
@@ -0,0 +1,60 @@
+﻿using Components.Support;
+using Components.Support.Tests.Entities;
+using FakeXrmEasy;
+using FluentAssertions;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Components.Support.Tests
+{
+    public class TagRegistrationPluginTests
+    {
+        public class when_tagging_an_entity
+        {
+            [Fact]
+            public void relationship_entity_should_be_created()
+            {
+                // Arrange
+                var context = new XrmFakedContext();
+
+                var tag = new Tag
+                {
+                    Id = Guid.NewGuid(),
+                    name = "tag"
+                };
+
+                var placeholder = new Placeholder
+                {
+                    Id = Guid.NewGuid(),
+                    tags = $"TAGDATA:{{\"relatedEntity\":\"tag\",\"relationshipName\":\"placeholder_tags\",\"tags\":[\"{tag.Id}\"]}}"
+                };
+
+                context.Initialize(new Entity[] { tag, placeholder });
+
+                // configure the relationship
+                context.AddRelationship("placeholder_tags", new XrmFakedRelationship
+                {
+                    IntersectEntity = PlaceholderTags.EntityLogicalName,
+                    Entity1LogicalName = Placeholder.EntityLogicalName,
+                    Entity1Attribute = "placeholderid",
+                    Entity2LogicalName = Tag.EntityLogicalName,
+                    Entity2Attribute = "tagid"
+                });
+
+                // Act
+                context.ExecutePluginWithTarget(new TagRegistrationPlugin(string.Empty, string.Empty), placeholder, PluginMessage.Create);
+
+                // Assert
+                var placeholderTags = context.CreateQuery<PlaceholderTags>().ToList();
+                placeholderTags.Should().ContainSingle();
+
+                var placeholderTag = placeholderTags.First();
+                placeholderTag.tagid.Should().Be(tag.Id);
+                placeholderTag.placeholderid.Should().Be(placeholder.Id);
+            }
+        }
+    }
+}
