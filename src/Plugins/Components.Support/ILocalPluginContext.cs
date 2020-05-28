8e92cfec6cdf24da52511174793cd1ae076c79af
Joshua Campbell <joshcampbell191@gmail.com>
2020-05-28 11:10:34 -0400

    Moved classes / interfaces into their own files.

---
+++ src/Plugins/Components.Support/ILocalPluginContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Components.Support
+{
+    /*
+     * This interface provides an abstraction on top of IServiceProvider for commonly used PowerApps CDS Plugin development constructs
+     */
+    public interface ILocalPluginContext
+    {
+        // The PowerApps CDS organization service for current user account
+        IOrganizationService CurrentUserService { get; }
+
+        // The PowerApps CDS organization service for system user account
+        IOrganizationService SystemUserService { get; }
+
+        // IPluginExecutionContext contains information that describes the run-time environment in which the plugin executes, information related to the execution pipeline, and entity business information
+        IPluginExecutionContext PluginExecutionContext { get; }
+
+        // Synchronous registered plugins can post the execution context to the Microsoft Azure Service Bus.
+        // It is through this notification service that synchronous plug-ins can send brokered messages to the Microsoft Azure Service Bus
+        IServiceEndpointNotificationService NotificationService { get; }
+
+        // Provides logging run time trace information for plug-ins.
+        ITracingService TracingService { get; }
+
+        // Writes a trace message to the CDS trace log
+        void Trace(string message);
+    }
+}
---
+++ src/Plugins/Components.Support/LocalTracingService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Components.Support
+{
+    /*
+     * Specialized ITracingService implementation that prefixes all traced messages with a time delta for Plugin performance diagnostics
+     */
+    public class LocalTracingService : ITracingService
+    {
+        private readonly ITracingService _tracingService;
+
+        private DateTime _previousTraceTime;
+
+        public LocalTracingService(IServiceProvider serviceProvider)
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            var context = (IExecutionContext)serviceProvider.GetService(typeof(IExecutionContext));
+
+            DateTime initialTimestamp = context.OperationCreatedOn;
+
+            if (initialTimestamp > utcNow)
+            {
+                initialTimestamp = utcNow;
+            }
+
+            _tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            _previousTraceTime = initialTimestamp;
+        }
+
+        public void Trace(string message, params object[] args)
+        {
+            var utcNow = DateTime.UtcNow;
+
+            // The duration since the last trace.
+            var deltaMilliseconds = utcNow.Subtract(_previousTraceTime).TotalMilliseconds;
+
+            _tracingService.Trace($"[+{deltaMilliseconds:N0}ms)] - {message}");
+
+            _previousTraceTime = utcNow;
+        }
+    }
+}
